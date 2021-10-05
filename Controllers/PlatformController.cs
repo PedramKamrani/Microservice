df897fb1d9d956605cf7bc67e71dc7a8c6a8e72b
PedramKamrani <kamranipedram@gmail.com>
2021-10-05 13:09:29 +0330

    Create

--- Controllers/PlatformController.cs
+++ Controllers/PlatformController.cs
@@ -4,9 +4,11 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using platformService.Data;
 using platformService.Dtos;
+using platformService.Models;
+
 namespace platformService.Controllers
 {
-   [Route("api/[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class PlatformController : ControllerBase
     {
@@ -26,14 +28,27 @@ namespace platformService.Controllers
             return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
         }
 
-         [HttpGet("{id}",Name="GetPlatformById")]
+        [HttpGet("{id}", Name = "GetPlatformById")]
         public ActionResult<PlatformReadDto> GetPlatformById(int id)
         {
-            
+
             var platforms = _repo.GetPlatformById(id);
-            if(platforms!=null)
-            return Ok(_mapper.Map<PlatformReadDto>(platforms));
+            if (platforms != null)
+                return Ok(_mapper.Map<PlatformReadDto>(platforms));
             return NotFound();
+        }
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatFormCreateDtos platFormCreateDtos)
+        {
+            var platformmodel=_mapper.Map<Platform>(platFormCreateDtos);
+            _repo.CreatePlatForm(platformmodel);
+            _repo.SaveChanges();
+
+
+            var platformReddto=_mapper.Map<PlatformReadDto>(platformmodel);
+
+            return CreatedAtRoute(nameof(GetPlatformById),new{Id = platformReddto.Id},platformReddto);
+
         }
     }
 }
